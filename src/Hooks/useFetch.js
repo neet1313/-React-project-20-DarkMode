3a6c3dfd5fd7de6bb42311065c1751a62d4fcfc6
Prabhneet Singh <prabhneet13@gmail.com>
2022-10-20 14:08:55 +0530

    Refactored Code

---
+++ src/Hooks/useFetch.js
@@ -0,0 +1,43 @@
+import { useState, useEffect, useCallback } from 'react'
+
+const API_ENDPOINT = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_MOVIE_API_KEY}`;
+
+export const useFetch = (urlParams) => {
+    const [isLoading, setIsLoading] = useState(true);
+    const [movies, setMovies] = useState(null);
+    const [error, setError] = useState({ show: false, msg: '' });
+
+    const fetchMovie = useCallback(async (url) => {
+        setIsLoading(() => true);
+
+        try {
+            const response = await fetch(url);
+            const data = await response.json();
+
+            if (data.Response === 'True') {
+                setMovies(data.Search || data);
+                setError({ show: false, msg: '' });
+
+            } else {
+                setError({ show: true, msg: data.Error });
+            }
+
+            setIsLoading(() => false);
+        } catch (error) {
+            console.log(error);
+            setIsLoading(() => true);
+        }
+    }, []);
+
+
+    useEffect(() => {
+        fetchMovie(`${API_ENDPOINT}${urlParams}`)
+    }, [urlParams, fetchMovie]);
+
+
+    return {
+        isLoading, error, movies
+    }
+}
+
+export default useFetch
--- src/SingleMovie.js
+++ src/SingleMovie.js
@@ -1,45 +1,12 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import { useParams, Link } from 'react-router-dom'
-import { API_ENDPOINT, useGlobalContext } from './context'
+import useFetch from './Hooks/useFetch'
 
-
-const url =
-  'https://upload.wikimedia.org/wikipedia/commons/f/fc/No_picture_available.png'
+const url = 'https://upload.wikimedia.org/wikipedia/commons/f/fc/No_picture_available.png'
 
 const SingleMovie = () => {
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState({ show: false, msg: '' });
-  const [movie, setmovie] = useState([]);
-
   const { id } = useParams();
-
-  const fetchmovie = async (url) => {
-    setIsLoading(() => true);
-
-    try {
-      const response = await fetch(url);
-      const data = await response.json();
-
-      if (data.Response === 'True') {
-        setmovie(() => data);
-        setError({ show: false, msg: '' });
-      } else {
-        setError({ show: true, msg: data.Error });
-      }
-
-      setIsLoading(() => false);
-
-    } catch (error) {
-      console.log(error);
-    }
-
-  }
-
-
-  useEffect(() => {
-    fetchmovie(`${API_ENDPOINT}&i=${id}`)
-  }, [id]);
-
+  const { movies, error, isLoading } = useFetch(`&i=${id}`);
 
 
   if (isLoading) {
@@ -53,7 +20,7 @@ const SingleMovie = () => {
     </div>
   }
 
-  const { Poster: poster, Title: title, Year: year, Plot: plot } = movie;
+  const { Poster: poster, Title: title, Year: year, Plot: plot } = movies;
   return <section className='single-movie'>
     <img src={poster !== 'N/A' ? poster : url} alt={title} />
     <div className='single-movie-info'>
